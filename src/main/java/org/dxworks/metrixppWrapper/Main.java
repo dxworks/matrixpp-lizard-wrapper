e685bcb238d022b79a167cd353ab4b69815139c6
DenisuDevForFood <denisfeier98@yahoo.com>
2021-02-08 22:49:06 +0200

    read csv from docker run

--- src/main/java/org/dxworks/metrixppWrapper/Main.java
+++ src/main/java/org/dxworks/metrixppWrapper/Main.java
@@ -2,6 +2,7 @@ package org.dxworks.metrixppWrapper;
 
 import com.opencsv.bean.CsvToBeanBuilder;
 import org.dxworks.metrixppWrapper.Entity.MetrixppOutput;
+import org.dxworks.metrixppWrapper.dockerRunner.DockerRunner;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -11,12 +12,14 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         System.out.println("metrix++ wrapper");
 
-        String fileName = "/Users/denisfeier/Documents/metrixPP-wrapper/src/main/resources/text.csv";
+        String fileName = "/Users/denisfeier/Documents/metrixPP-wrapper/results/test.csv";
+
+        DockerRunner.execute();
 
         List beans = new CsvToBeanBuilder(new FileReader(fileName))
-                .withType(MetrixppOutput.class)
-                .build()
-                .parse();
+                        .withType(MetrixppOutput.class)
+                        .build()
+                        .parse();
 
         beans.forEach(System.out::println);
 
